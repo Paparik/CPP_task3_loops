e7bd80652086a1349685defaf2945c81f5547255
Paparik <82972576+Paparik@users.noreply.github.com>
2024-11-11 21:54:11 +0300

    my first time with cpp👉🏿👈🏿

---
+++ task1.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+
+int main()
+{
+    float credit_amount;
+    int procent, profit;
+    int year = 0, total_money = 0;
+
+    std::cout << "Input credit amount: ";
+    std::cin >> credit_amount;
+    std::cout << "Input percentage rate: ";
+    std::cin >> procent;
+    std::cout << "Input profit for year: ";
+    std::cin >> profit;
+
+
+    int prec = credit_amount / 100 * procent;
+
+    if (prec >= profit)
+        std::cout << "It will not be possible to repay the credit";
+    else
+    {
+        while (total_money < credit_amount)
+        {
+            credit_amount += prec;
+            total_money += profit;
+            ++year;
+        }
+        std::cout << "Credit can be repaid in " << year << " year";
+    }
+}
---
+++ task2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main() {
+    int time;
+    int total_time = 0;
+
+    std::cout << "Input processing time for each part:";
+
+    while (std::cin >> time) 
+    {
+        total_time += time;
+    }
+
+    std::cout << "Total processing time for all parts: " << total_time;
+}
