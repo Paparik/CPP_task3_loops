7e4d16049b7989d86558e2534d1e9d2214fcd1f8
Paparik <82972576+Paparik@users.noreply.github.com>
2024-11-25 08:10:04 +0300

    upd

---
+++ task3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+
+int main() {
+    int T;
+    std::cout << "Input T: ";
+    std::cin >> T;
+
+    int n;
+    std::cout << "Input n: ";
+    std::cin >> n;
+
+    std::vector<int> arrival(n);
+    std::vector<int> duration(n);
+
+    for (int i = 0; i < n; ++i) {
+        std::cout << "Input t and l " << i + 1 << ": ";
+        std::cin >> arrival[i] >> duration[i];
+    }
+
+    int servedClients = 0;
+    int totalServiceTime = 0;
+    int currentTime = 0;
+
+    for (int i = 0; i < n; ++i) {
+        if (arrival[i] >= currentTime && arrival[i] + duration[i] <= T) {
+            servedClients++;
+            totalServiceTime += duration[i];
+            currentTime = arrival[i] + duration[i];
+        }
+    }
+
+    std::cout << "Clients served: " << servedClients;
+    std::cout << "Working time of the master: " << totalServiceTime;
+}
---
+++ task4.cpp
@@ -0,0 +1,41 @@
+﻿#include <iostream>
+
+int main() {
+    double u, v, w;
+    int k;
+    double s, t, f;
+    double p, q, r, z;
+
+    std::cout << "Input u v w k s t f p q r z: ";
+    std::cin >> u >> v >> w >> k >> s >> t >> f >> p >> q >> r >> z;
+
+    int day = 0;
+
+    while (true) {
+        double daily_hay = u * k;
+        double daily_silage = v * k;
+        double daily_feed = w * k;
+
+        if (s < daily_hay || t < daily_silage || f < daily_feed) {
+            std::cout << "Day: " << day;
+            if (s < daily_hay) 
+                std::cout << "Hay ends first" << std::endl;
+            else if (t < daily_silage) 
+                std::cout << "Silage ends first" << std::endl;
+            else 
+                std::cout << "Feed ends first";
+            break;
+        }
+
+        s -= daily_hay;
+        t -= daily_silage;
+        f -= daily_feed;
+
+        s -= s * q / 100;
+        t -= t * r / 100;
+        f -= f * z / 100;
+
+        k -= k * p / 100;
+        day++;
+    }
+}
